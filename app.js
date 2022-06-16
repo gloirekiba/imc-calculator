ef60ebdbba5cee372b1eb5dbb3592031d57f1357
Gloire Kiba <kibagloire1530@gmail.com>
2022-06-16 16:35:02 +0200

    ✨ add change image based on the imc range

--- app.js
+++ app.js
@@ -8,19 +8,20 @@ const form = document.getElementById("form");
 function getImcRange(imc) {
   switch (true) {
     case imc < 18.5:
-      return 0;
+      return 0; // Insuffisance pondérale (maigreur)
     case imc >= 18.5 && imc <= 25:
-      return 1;
+      return 1; // Corpulence normale
     case imc >= 25 && imc <= 30:
-      return 2;
+      return 2; // Surpoids
     case imc >= 30 && imc <= 35:
-      return 3;
+      return 3; // Obésité modérée
     case imc >= 35 && imc <= 40:
-      return 4;
+      return 4; // Obésité sévère
     case imc >= 40:
-      return 5;
+      return 5; // Obésité morbide ou massive
     default:
-      return undefined;
+      alert("Invalid IMC");
+      throw new Error("Invalid IMC"); // Invalid IMC
   }
 }
 
@@ -42,6 +43,12 @@ function displayResult(imc) {
   const imcRange = getImcRange(imc);
   resultTitle.innerText = resultText[imcRange];
   resultValue.innerText = imc;
+
+  if (imcRange >= 5) {
+    return img.setAttribute("src", `src/img${4}.png`);
+  }
+
+  img.setAttribute("src", `src/img${imcRange}.png`);
 }
 
 form.addEventListener("submit", (e) => {
